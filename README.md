ed995559d13a9caa104080c5beea788452512fb9
leondgarse <leondgarse@gmail.com>
2021-05-14 15:59:48 +0800

    upload mlp mixer

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Keras_mlp
+***
+
+## MLP mixer
+  - [PDF MLP-Mixer: An all-MLP Architecture for Vision](https://arxiv.org/pdf/2105.01601.pdf)
+  - [Github lucidrains/mlp-mixer-pytorch](https://github.com/lucidrains/mlp-mixer-pytorch)
+  - [Github Benjamin-Etheredge/mlp-mixer-keras](https://github.com/Benjamin-Etheredge/mlp-mixer-keras)
+  - **Calculate total parameters** `DS -> token-mixing, DC --> channel-mixing`
+    ```py
+    inputs, patch_size, hidden_dim, DS, DC, num_blocks = 224, 32, 512, 256, 2048, 8
+    num_patch = (inputs * inputs) // (patch_size * patch_size)
+    conv = (patch_size * patch_size * 3 + 1) * hidden_dim
+    mlp_1 = (num_patch + 1) * DS + (DS + 1) * num_patch # input_shape = (num_patch, hidden_dim)
+    mlp_2 = (hidden_dim + 1) * DC + (DC + 1) * hidden_dim
+    mixer = (2 * num_patch) * 2 + mlp_1 +  mlp_2  # mixer: (LN = 2 * num_patch) * 2 + mlp_1 +  mlp_2
+    total = mixer * num_blocks + conv
+    print(f'{total = }')  # total = 18575784
+    ```
+
+    | Specification        | S/32  | S/16  | B/32  | B/16  | L/32  | L/16  | H/14  |
+    | -------------------- | ----- | ----- | ----- | ----- | ----- | ----- | ----- |
+    | Number of layers     | 8     | 8     | 12    | 12    | 24    | 24    | 32    |
+    | Patch resolution P×P | 32×32 | 16×16 | 32×32 | 16×16 | 32×32 | 16×16 | 14×14 |
+    | Hidden size C        | 512   | 512   | 768   | 768   | 1024  | 1024  | 1280  |
+    | Sequence length S    | 49    | 196   | 49    | 196   | 49    | 196   | 256   |
+    | MLP dimension DC     | 2048  | 2048  | 3072  | 3072  | 4096  | 4096  | 5120  |
+    | MLP dimension DS     | 256   | 256   | 384   | 384   | 512   | 512   | 640   |
+    | Parameters (M)       | 10    | 10    | 46    | 46    | 188   | 189   | 409   |
+    | Self defined         | 18.5  | 18.0  | 59.5  | 59.1  | 205.8 | 207.1 | 430.9 |
+
+  - **Pre-training details**
+    - We pre-train all models using Adam with β1 = 0.9, β2 = 0.999, and batch size 4 096, using weight decay, and gradient clipping at global norm 1.
+    - We use a linear learning rate warmup of 10k steps and linear decay.
+    - We pre-train all models at resolution 224.
+    - For JFT-300M, we pre-process images by applying the cropping technique from Szegedy et al. [44] in addition to random horizontal flipping.
+    - For ImageNet and ImageNet-21k, we employ additional data augmentation and regularization techniques.
+    - In particular, we use RandAugment [12], mixup [56], dropout [42], and stochastic depth [19].
+    - This set of techniques was inspired by the timm library [52] and Touvron et al. [46].
+    - More details on these hyperparameters are provided in Supplementary B.
+***
---
+++ mlp_mixer.py
@@ -0,0 +1,80 @@
+from tensorflow import keras
+
+
+def mlp_block(inputs, hidden_dim, activation="gelu", name=None):
+    nn = keras.layers.Dense(hidden_dim, name=name + "dense_1")(inputs)
+    nn = keras.layers.Activation(activation, name=name + "gelu")(nn)
+    nn = keras.layers.Dense(inputs.shape[-1], name=name + "dense_2")(nn)
+    return nn
+
+
+def mixer_block(inputs, tokens_mlp_dim, channels_mlp_dim=None, activation=None, name=None):
+    nn = keras.layers.LayerNormalization(axis=1, name=name + "layer_norm_1")(inputs)
+    nn = keras.layers.Permute((2, 1), name=name + "permute_1")(nn)
+    nn = mlp_block(nn, tokens_mlp_dim, activation, name=name + "mlp_1_")
+    nn = keras.layers.Permute((2, 1), name=name + "permute_2")(nn)
+    skip_conn = keras.layers.Add(name=name + "add_1")([nn, inputs])
+
+    nn = keras.layers.LayerNormalization(axis=1, name=name + "layer_norm_2")(skip_conn)
+    nn = mlp_block(nn, channels_mlp_dim, activation, name=name + "mlp_2_")
+    return keras.layers.Add(name=name + "add_2")([nn, skip_conn])
+
+
+def MlpMixerModel(
+    input_shape,
+    num_blocks,
+    patch_size,
+    hidden_dim,
+    tokens_mlp_dim,
+    channels_mlp_dim,
+    num_classes=0,
+    classifier_activation="softmax",
+    model_name="MlpMixer",
+):
+    inputs = keras.Input(input_shape)
+    nn = keras.layers.Conv2D(hidden_dim, kernel_size=patch_size, strides=patch_size, padding="same", name="projector")(inputs)
+    nn = keras.layers.Reshape([-1, hidden_dim])(nn)
+
+    for ii in range(num_blocks):
+        name = "_".join(["mixer_block", str(ii), ""])
+        nn = mixer_block(nn, tokens_mlp_dim=tokens_mlp_dim, channels_mlp_dim=channels_mlp_dim, name=name)
+
+    if num_classes > 0:
+        nn = keras.layers.GlobalAveragePooling1D()(nn)  # tf.reduce_mean(nn, axis=1)
+        nn = keras.layers.LayerNormalization(name="pre_head_layer_norm")(nn)
+        nn = keras.layers.Dense(num_classes, activation=classifier_activation, name="predictions")(nn)
+    return keras.Model(inputs, nn, name=model_name)
+
+
+def MlpMixerModel_S32(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(input_shape, 8, 32, 512, 256, 2048, num_classes, classifier_activation, "MlpMixerModel_S32", **kwargs)
+
+
+def MlpMixerModel_S16(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(input_shape, 8, 16, 512, 256, 2048, num_classes, classifier_activation, "MlpMixerModel_S16", **kwargs)
+
+
+def MlpMixerModel_B32(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(input_shape, 12, 32, 768, 384, 3072, num_classes, classifier_activation, "MlpMixerModel_B32", **kwargs)
+
+
+def MlpMixerModel_B16(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(input_shape, 12, 16, 768, 384, 3072, num_classes, classifier_activation, "MlpMixerModel_B16", **kwargs)
+
+
+def MlpMixerModel_L32(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(
+        input_shape, 24, 32, 1024, 512, 4096, num_classes, classifier_activation, "MlpMixerModel_L32", **kwargs
+    )
+
+
+def MlpMixerModel_L16(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(
+        input_shape, 24, 16, 1024, 512, 4096, num_classes, classifier_activation, "MlpMixerModel_L16", **kwargs
+    )
+
+
+def MlpMixerModel_H14(input_shape=(224, 224, 3), num_classes=0, classifier_activation="softmax", **kwargs):
+    return MlpMixerModel(
+        input_shape, 32, 14, 1280, 640, 5120, num_classes, classifier_activation, "MlpMixerModel_H14", **kwargs
+    )
